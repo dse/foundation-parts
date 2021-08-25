774640ce7f7af495129f35714dc17e41bfd9f5ca
Darren Embry <dse@webonastick.com>
2021-08-25 19:26:07 -0400

    revised build script

--- bin/build.sh
+++ bin/build.sh
@@ -5,106 +5,91 @@ set -o nounset
 shopt -s lastpipe
 
 MAIN () {
-    rm -frv scss/components/*.scss || true
-    rm -frv css/components/*.css || true
+    rm -frv scss || true
+    rm -frv css || true
+    mkdir scss
+    mkdir css
 
-    gen foundation-normalize       foundation-000-normalize     # separate from the rest of foundation-global-styles
-    gen foundation-global-styles   foundation-001-global-styles # does not include normalize
-    gen foundation-forms           foundation-002-forms
-    gen foundation-typography      foundation-003-typography
+    declare -a components
+    sed -n '/^@include /{s///;s/;$//;p;}' <<EOF | mapfile -t components
+// Global styles
+@include foundation-normalize;
+@include foundation-global-styles;
+@include foundation-forms;
+@include foundation-typography;
+@include foundation-grid;
+@include foundation-xy-grid-classes;
+@include foundation-flex-grid;
 
-    # Grids (choose one)
-    gen foundation-xy-grid-classes foundation-100-xy-grid
-    gen foundation-grid            foundation-101-grid
-    gen foundation-flex-grid       foundation-102-flex-grid
+// Generic components
+@include foundation-button;
+@include foundation-button-group;
+@include foundation-close-button;
+@include foundation-label;
+@include foundation-progress-bar;
+@include foundation-slider;
+@include foundation-switch;
+@include foundation-table;
+// Basic components
+@include foundation-badge;
+@include foundation-breadcrumbs;
+@include foundation-callout;
+@include foundation-card;
+@include foundation-dropdown;
+@include foundation-pagination;
+@include foundation-tooltip;
 
-    # Generic components
-    gen foundation-button
-    gen foundation-button-group
-    gen foundation-close-button
-    gen foundation-label
-    gen foundation-progress-bar
-    gen foundation-slider
-    gen foundation-switch
-    gen foundation-table
+// Containers
+@include foundation-accordion;
+@include foundation-media-object;
+@include foundation-orbit;
+@include foundation-responsive-embed;
+@include foundation-tabs;
+@include foundation-thumbnail;
+// Menu-based containers
+@include foundation-menu;
+@include foundation-menu-icon;
+@include foundation-accordion-menu;
+@include foundation-drilldown-menu;
+@include foundation-dropdown-menu;
 
-    # Basic components
-    gen foundation-badge
-    gen foundation-breadcrumbs
-    gen foundation-callout
-    gen foundation-card
-    gen foundation-dropdown
-    gen foundation-pagination
-    gen foundation-tooltip
+// Layout components
+@include foundation-off-canvas;
+@include foundation-reveal;
+@include foundation-sticky;
+@include foundation-title-bar;
+@include foundation-top-bar;
 
-    # Containers
-    gen foundation-accordion
-    gen foundation-media-object
-    gen foundation-orbit
-    gen foundation-responsive-embed
-    gen foundation-tabs
-    gen foundation-thumbnail
+// Helpers
+@include foundation-float-classes;
+@include foundation-flex-classes;
+@include foundation-visibility-classes;
+@include foundation-prototype-classes;
+EOF
 
-    # Menu-based containers
-    gen foundation-menu
-    gen foundation-menu-icon
-    gen foundation-accordion-menu
-    gen foundation-drilldown-menu
-    gen foundation-dropdown-menu
-
-    # Layout components
-    gen foundation-off-canvas
-    gen foundation-reveal
-    gen foundation-sticky
-    gen foundation-title-bar
-    gen foundation-top-bar
-
-    # Helpers
-    gen foundation-float-classes
-    gen foundation-flex-classes
-    gen foundation-visibility-classes
-    gen foundation-prototype-classes
-
-    # Motion UI
-    gen motion-ui-transitions
-    gen motion-ui-animations
-
-    for sassfile in scss/components/*.scss ; do
-        cssfile="${sassfile##*/}"
-        cssfile="css/components/${cssfile%.scss}.css"
-        mkdir -p "$(dirname "${cssfile}")"
-        sassc -I scss:node_modules/foundation-sites/scss:node_modules/motion-ui/src \
-              -t expanded "${sassfile}" "${cssfile}"
+    declare -i counter=0
+    for component in "${components[@]}" ; do
+        counter+=1
+        xxx="$(printf "%03d" "${counter}")"
+        sassfile="scss/${xxx}-${component}.scss"
+        cssfile="css/${xxx}-${component}.css"
+        >&2 echo "${component} - ${sassfile} ${cssfile}"
+        if [[ "${component}" = "foundation-global-styles" ]] ; then
+            cat <<EOF >"${sassfile}"
+@charset 'utf-8';
+@import "foundation";
+@mixin foundation-normalize {}
+@include ${component};
+EOF
+        else
+            cat <<EOF >"${sassfile}"
+@charset 'utf-8';
+@import "foundation";
+@include ${component};
+EOF
+        fi
+        sassc -t expanded -I node_modules/foundation-sites/scss "${sassfile}" "${cssfile}"
     done
-}
-
-gen () {
-    component="$1"
-    if (( $# >= 2 )) ; then
-        filename="$2"
-    else
-        filename="${component}"
-    fi
-    mkdir -p scss/components
-    scssfile="scss/components/${filename}.scss"
-    >&2 echo "generating ${scssfile}"
-
-    # We exclude normalize from the foundation-global-styles.css we
-    # generate, because I wanted normalize in a separate file.
-    if [[ "${component}" = "foundation-global-styles" ]] ; then
-        cat <<END >"${scssfile}"
-@charset 'utf-8';
-@import 'prerequisites';
-@mixin foundation-normalize { }
-@include ${component};
-END
-    else
-        cat <<END >"${scssfile}"
-@charset 'utf-8';
-@import 'prerequisites';
-@include ${component};
-END
-    fi
 }
 
 ###############################################################################
